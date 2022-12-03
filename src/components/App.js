a05c1ce1f520a46699ee2ade0adc03d4877fa528
Ahmad Al Hariri <ahmadmhd357@gmail.com>
2022-12-03 22:35:01 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
@@ -6,6 +6,19 @@ import PetBrowser from "./PetBrowser";
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+  const fetchData = async () => {
+    try {
+      const URL = filters.type === 'all'?`http://localhost:3001/pets`:`http://localhost:3001/pets?type=${filters.type}`
+      const response = await fetch(URL);
+      const data = await response.json();
+      setPets(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div className="ui container">
@@ -15,10 +28,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters setFilters={setFilters} fetchData={fetchData} />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} setPets={setPets} />
           </div>
         </div>
       </div>
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -2,8 +2,15 @@ import React from "react";
 
 import Pet from "./Pet";
 
-function PetBrowser() {
-  return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>;
+function PetBrowser({ pets, setPets }) {
+  return (
+    <div className="ui cards">
+      {pets?.map((singlePet) => {
+        const { id } = singlePet;
+        return <Pet key={id} data={singlePet} petId={id} setPets={setPets} />;
+      })}
+    </div>
+  );
 }
 
 export default PetBrowser;
